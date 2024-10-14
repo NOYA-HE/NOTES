ecf93e6aa3700246214d6748b04faf0108629057
NOYA-HE <noya.he.26@gmail.com>
2024-10-13 21:52:07 -0600

    INPUT AND OUTPUT

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -1,18 +1,46 @@
 import org.w3c.dom.ls.LSOutput;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        /* DECLARACION DE VARIABLES
+        int x = 123;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String name = "Bro";
 
-        String name = JOptionPane.showInputDialog("Enter your name bitch");
-        JOptionPane.showMessageDialog(null,"Hello "+name);
+        System.out.println("Hello "+name);
+        */
 
-        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
-        JOptionPane.showMessageDialog(null, "You are "+age+" years old");
+        /* SWAP TWO VARIABLES
+        String x = "Water";
+        String y = "Kool-Aid";
+        String temp;
 
-        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
-        JOptionPane.showMessageDialog(null, "You are" +height+" cm tall");
+        temp = x;
+        x = y;
+        y = temp;
 
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+        */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name bitch? ");
+        String name = scanner.nextLine();
+
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("What is your favorite food? ");
+        String food = scanner.nextLine();
+
+        System.out.println("Hello "+name);
+        System.out.println("You are "+age+" years old");
+        System.out.println("You like "+food);
     }
 }
